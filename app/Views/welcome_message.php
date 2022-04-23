9bad64e25176d33b8342d7f610269c18bb5f65a9
Sawalinto <sawalinto@gmail.com>
2022-04-24 05:43:37 +0700

    first commit

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+	<title>tugas pemrograman web</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	
+	<!-- bagian header template -->
+	<header>
+		<h1 class="judul">Tugas Web </h1>
+		<p class="deskripsi"> Belajar Pemrograman Web</p>
+	</header>
+	<!-- akhir bagian header template -->
+	
+	<div class="wrap">
+		<!-- bagian menu		 -->
+		<nav class="menu">
+			<ul>
+				<li>
+					<a href="#">Home</a>
+				</li>
+				<li>
+					<a href="#">Tutorial</a>
+				</li>
+				<li>
+					<a href="#">Kontak</a>
+				</li>
+			</ul>
+		</nav>
+		<!-- akhir bagian menu -->
+ 
+		<!-- bagian sidebar website -->
+		<aside class="sidebar">
+			<div class="widget">
+				<h2>Welcome</h2>
+				<p>Selamat datang di Website Pribadiku, Website ini berisikan Biodataku dan Belajar HTML.</p>
+			</div>
+			<div class="widget">
+				<h2>Sosial Mediaku</h2>
+				<p>Teman-teman bisa Follow Akun Sosial Media saya</p>
+					 <p><a target="_blank" href="https://web.facebook.com/jirayuras.jirayuras/"><img src="facebook.png"></a>
+				<a target="_blank" href="https://www.instagram.com/_mrasyidi_/?hl=id"><img src="instagram.jpg"></a>.</p>
+			</div>
+		</aside>
+		<!-- akhir bagian sidebar website -->
+ 
+		<!-- bagian konten Blog -->
+		<div class="blog">
+			<div class="conteudo">
+				<div class="post-info">
+					Created by <b>Muhammad Rasyidi</b>
+				</div>
+				<img src="111.jpg">
+				<h1> Biodata Pribadiku </h1>
+				<hr>
+				<p>
+					Saya adalah Mahasiswa Triguna Dharma semester 4, jurusan Sistem Informasi. Tempat tinggal saya di Labuhanbatu Selatan Kec. Kampung Rakyat tepatnya di Desa Tanjung Mulia.
+					tempat yang sangat jauh dari kota. kampung yg hampir berbatasan dengan provinsi Riau. Saya masuk Triguna Dharma ingin belajar lebih dalam mengenai   dunia perdigitalan. Terima Kasih
+				</p>				
+				<a href="#" class="continue-lendo">Selengkapnya →</a><br><br>
+
+				<h1>PETA KAMPUS TRIGUNA DHARMA</h1>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3982.1992765850923!2d98.6581617138102!3d3.5414444974226997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30313a7e757a80cf%3A0xd4e3a9dc3a116b97!2sSTMIK%20Triguna%20Dharma!5e0!3m2!1sid!2sid!4v1650442325969!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			</div>
+			<div class="conteudo">
+				<div class="post-info">
+					Created by <b>Muhammad Rasyidi</b>
+				</div>
+				<img src="11.jpg">
+				<h1> Belajar HTML  </h1>
+				<hr>
+				<p>
+					Untuk belajar pemrograman Web ini, ada beberapa hal yang perlu kita siapkan, yang pertama adalah laptop, kemudian software dan user. 
+					untuk belajarnya sendiri kita bisa otodidak atau bisa juga les privat. tergantung kita sendiri. Kalau saya lebih suka belajar bersama teman atau ada guru yang mengajari secara langsung
+				</p>				
+				<a href="#" class="continue-lendo">Selengkapnya →</a>
+			</div>
+		</div>
+		<!-- akhir bagian konten Blog -->
+	</div>
+
+    </body>
+</html>
